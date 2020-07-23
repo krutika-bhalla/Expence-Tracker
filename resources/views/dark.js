8002562b94be0d84190632af61f0c5bbd89a49f8
DhairyaMehta2000 <65503689+DhairyaMehta2000@users.noreply.github.com>
2020-07-23 13:01:37 +0530

    footer and dark mode and bg img

---
+++ resources/views/dark.js
@@ -0,0 +1,19 @@
+const options = {
+  bottom: 'unset', // default: '32px'
+  right: '32px', // default: '32px'
+  left: '32px', // default: 'unset'
+  time: '0.1s', // default: '0.3s'
+  mixColor: '#dbe6c8', // default: '#fff'
+  backgroundColor: '#fff',  // default: '#fff'
+  buttonColorDark: '#100f2c',  // default: '#100f2c'
+  buttonColorLight: '#fff', // default: '#fff'
+  saveInCookies: true, // default: true,
+  label: '🌓', // default: ''
+  autoMatchOsTheme: true // default: true
+}
+const darkmode = new Darkmode(options);
+darkmode.showWidget();
+// #2F2F2F // grey
+
+// #bde06c closest
+// #e8eddf2nd #dee8ca1st
